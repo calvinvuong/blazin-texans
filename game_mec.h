559a0552155b842b2e59d3055ce0e859776a6961
gTopa <gtopa@stuy.edu>
2017-01-09 18:43:55 -0500

    started defining stuff

--- game_mec.h
+++ game_mec.h
@@ -5,9 +5,22 @@ typedef struct player{
   int score;
   int playerNumber;
   //socket or whatever
+  //IP thingy
 } player;
+
 typedef struct card{
   int num;
   char suit;
 } card;
 
+char deck[52][4]={"AC","AS","AH","AD","2C","2S","2H","2D","3C","3S","3H","3D","4C","4S","4H","4D","5C","5S","5H","5D","6C","6S","6H","6D","7C","7S","7H","7D","8C","8S","8H","8D","9C","9S","9H","9D","10C","10S","10H","10D","JC","JS","JH","JD","QC","QS","QH","QD","KC","KS","KH","KD"};
+  
+int deal(char * deck, struct player * players);
+
+int bet(int amount, struct player);
+
+int call(struct player);
+
+int check(struct player);
+
+int fold(struct player);
