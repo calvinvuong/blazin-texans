3a74eebe98b5ecdd8a6e4ef4fa891b465b80cad6
Calvin Vuong <cvuong@stuy.edu>
2017-01-09 21:07:41 -0500

    started networking.h

---
+++ networking.h
@@ -0,0 +1,12 @@
+
+
+// data sent to the IP of a specific person
+// person's IP speicified outside of this struct
+typedef struct to_client {
+  char label[500];
+};
+
+typedef struct from_client {
+  char label[500];
+};
+
