027601905dab4b37006cb059e0cfb0035e35f996
Calvin Vuong <cvuong@stuy.edu>
2017-01-13 15:35:16 -0500

    started writing server code

--- server.c
+++ server.c
@@ -3,8 +3,22 @@
 
 int main() {
   
+  int sd, connection;
+  sd = server_setup();
 
-
+  while (1) {
+    connection = server_connect(sd);
+    
+    // read from connection to get client ip and port
+    int f = fork();
+    if ( f == 0 ) {
+      close(sd);
+      exit(0);
+    }
+    else
+      close(connection);
+  }
+    
 
 
   return 0;
