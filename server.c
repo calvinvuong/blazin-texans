0eb37ff097951408e6cecf65b93d940b7772ce1f
Calvin Vuong <cvuong@stuy.edu>
2017-01-16 16:30:18 -0500

    implemented ability to receive connections while game is going on and queue them for the next game

--- server.c
+++ server.c
@@ -90,14 +90,17 @@ int main() {
   unsigned int ip_queue[20];
   int queue_size = 0;
   int game_running = 0; // 0 if no game running; 1 if there is
-  int game_pid;
+  int game_pid = 90;
   
   sd = server_setup(1379);
   
   while (1) {
     listener(sd, ip_queue, &queue_size);
+
+    printf("game_pid: %d\n", game_pid);
     game_running = update_game_status(game_pid);
-    
+    printf("game_running: %d\n", game_running);
+
     if ( queue_size > 1 && game_running == 0 ) { // ready to start game
       unsigned int player_IPs[4];
       int num_players;
@@ -108,29 +111,15 @@ int main() {
 
       if ( game_running == 0 ) { // no game running
 	game_running = 1;
-	int f = fork();
-	if ( f == 0 )  // child process
+	game_pid = fork();
+	signal(SIGCHLD, SIG_IGN); // circumvents waiting
+	if ( game_pid == 0 ) {  // child process
 	  game(player_IPs, num_players); // start game
-	else
-	  game_pid = f; // store pid of the game / child process
+	  printf("hi\n");
+	  exit(0);
+	}
       }
-
     }
-    // if only 1 player connected, pass
-    
-    /*
-    connection = initial_server_connect(sd, &(ip_queue[queue_size]));
-    queue_size++;
-    
-    char buffer[500];
-    read( connection, &buffer, sizeof(buffer) );
-    if ( strcmp(buffer, "client to listener") == 0 )
-      write( connection, "received client to listener; please standby", sizeof("received client to listener; please standby") );
-    close(connection);
-
-    
-    print_IP(ip_queue, queue_size);
-    */
   }    
 
   return 0;
