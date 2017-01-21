2932ae1011440fab7f56ddc68cb2c1d565db1c90
gTopa <gtopa@stuy.edu>
2017-01-21 15:17:20 -0500

    work

--- server.c
+++ server.c
@@ -171,7 +171,27 @@ int main() {
 	game_pid = fork();
 	signal(SIGCHLD, SIG_IGN); // circumvents waiting
 	if ( game_pid == 0 ) {  // child process
-	  game(player_IPs, num_players); // start game
+	  while(num_players>1){
+	    struct card * deck=(struct card *) malloc(sizeof(struct card)*53);
+	    struct card * river=(struct card *) malloc(sizeof(struct card)*5);
+	    int len_river=0;
+	    int highest_bet=0;
+	    makeDeck(deck);
+	    shuffle(deck);
+	    deal(players, num_players, deck, 2);
+	    betting(players, *highest_bet, num_players, river, len_river);
+	    addCards(river, 3, *len_river);
+	    betting(players, *highest_bet, num_players, river, len_river);
+	    addCards(river, 1, *len_river);
+	    betting(players, *highest_bet, num_players, river, len_river);
+	    addCards(river, 1, *len_river);
+	    betting(players, *highest_bet, num_players, river, len_river);
+	    //score_players();
+	    //find_max();
+	    //move_money();
+	    //check_if_broke();
+	  }
+	  //game(player_IPs, num_players); // start game
 	  printf("hi\n");
 	  exit(0);
 	}
